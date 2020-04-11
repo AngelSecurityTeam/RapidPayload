c89b3e1b94dd9846b8d5341c06a20b8ff6e0c995
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-04-11 12:54:46 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,5 @@
 pip3 install -r requirements.txt
 python3 Server.py
 sudo chmod +x RapidPayload.py
-apt-get install wine # install default  [ wine ]| [ wine64 ] 
+apt-get install wine # install default  [ wine ]| [ wine64 ]
+apt-get install osslsigncode # osslsigncode
