c8ebaaa6501073f117a89afc380f1a734d37848d
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-04-10 19:08:32 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,5 @@
 #https://github.com/AngelSecurityTeam
 pip3 install -r requirements.txt
 python3 Server.py
-sudo chmod +x RapidPayload.py 
+sudo chmod +x RapidPayload.py
+apt install wine # install wine
