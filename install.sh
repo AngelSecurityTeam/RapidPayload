a451e2b4c8b9b209a21934d69a038d3b4d35d696
AngelSecurityTeam <55148534+AngelSecurityTeam@users.noreply.github.com>
2020-04-11 16:14:44 -0500

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,5 +2,10 @@
 pip3 install -r requirements.txt
 python3 Server.py
 sudo chmod +x RapidPayload.py
+var=$(pwd)
+cd  $var"/Image/"
+unzip Hyperion.zip
+rm -rf Hyperion.zip
+cd ..
 apt-get install wine # install default  [ wine ]| [ wine64 ]
 apt-get install osslsigncode # osslsigncode
